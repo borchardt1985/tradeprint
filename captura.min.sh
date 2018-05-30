2579d1aff8ec0c7fe5a33c21c7df05af0e04d64e
Nelson Ariberto Borchardt <39770059+borchardt1985@users.noreply.github.com>
2018-05-30 17:07:21 -0300

    Update captura.min.sh

--- captura.min.sh
+++ captura.min.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #AUTOR JORGE DIAS jorgediasdsg@gmail.com https://github.com/jorgedsdsg/captura
-cd $HOME/TRADE/captura
+cd $HOME/TRADE/tradeprint
 e(){ echo $1; }; p(){ clear; seq 7 8 180 | paste -sd \X; e ""; e " SISTEMA CAPTURA DE TELA E ENVIO DE EMAIL"; e ""; e "	$1"; e ""; seq 7 8 180 | paste -sd \X; }; d=$(date +%d-%m-%Y-%X); s=sites.txt; m=emails.txt; l=`pwd`; w=google-chrome-stable; a=chrome
 r(){ grep -v "^#" $s > g; while read n k; do d=$(date +%d-%m-%Y-%X); $w $k; e "$n - $d - $k" >> r; sleep 12; gnome-screenshot -f $l/$n-$d.png; done < g; killall $a; }
 t(){ e "-----------------------------------------------" > $l/c; e "RELATORIO DE SITES ACESSADOS" >> $l/c; e "" >> $l/c; cat r >> $l/c; e "" >> $l/c; e "" >> $l/c; e "" >> $l/c; e "ATT NELSON BORCHARDT" >> $l/c; e "-----------------------------------------------" >> $l/c; }
